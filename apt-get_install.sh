bb5c76d008bc1a20ef0b3e33338739b701767b81
craig8196 <craig.crj@gmail.com>
2015-04-28 12:59:18 -0600

    Updated install scripts.

--- apt-get_install.sh
+++ apt-get_install.sh
@@ -8,23 +8,31 @@ apt-get -y install synaptic software-center
 apt-get -y install vim geany
 # Install firefox
 apt-get -y install firefox
-# Install chrome
-sudo apt-get install libxss1 libappindicator1 libindicator7
-sudo apt-get install libnss3-nssdb libnss3 libnspr4
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
 # Python
 apt-get -y install python python3 python-pip python-numpy python-scipy
+apt-get -y install ipython
 # Java
 
-# R
-# sudo apt-get -y install r-base r-base-core
+
 # Audio
 apt-get -y install audacity
 # Video
 apt-get -y install vlc
+#Image
+apt-get -y gimp
 # Office Productivity
 apt-get -y install libreoffice
+
+###### These were for classes, but are no longer needed. ######
 # Spin Prerequisites
 # apt-get -y install byacc flex
+# R
+# sudo apt-get -y install r-base r-base-core
 
+
+# Clean up
+echo "Cleaning up." &&
+sudo apt-get -f install &&
+sudo apt-get autoremove &&
+sudo apt-get -y autoclean &&
+sudo apt-get -y clean
