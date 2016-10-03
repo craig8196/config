5b9b1ad85bf246d2d28a9bdcb39631427b39c718
v <craig.crj@gmail.com>
2016-10-02 18:56:00 -0600

    Uncommented java install and cleaned up.

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -5,18 +5,12 @@
 # Update before proceeding
 apt-get update
 
-<<<<<<< HEAD
-
-# Preferred Terminal
-apt-get -y install terminator
-=======
->>>>>>> 622d2ec23a6f63ba6d87afddd447c4c31dd61161
 # Software finders
 apt-get -y install synaptic software-center
 # Text editors
 apt-get -y install vim
 # Install firefox
-apt-get -y install firefox
+#apt-get -y install firefox
 # Python
 apt-get -y install python python3 python-pip python-numpy python-scipy
 apt-get -y install ipython
@@ -27,12 +21,12 @@ apt-get -y install vlc
 # Image
 apt-get -y gimp
 # Office Productivity
-apt-get -y install libreoffice
+#apt-get -y install libreoffice
 # Haskell
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:hvr/ghc
-sudo apt-get update
-sudo apt-get install -y cabal-install-1.22 ghc-7.10.3
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:hvr/ghc
+apt-get update
+apt-get install -y cabal-install-1.22 ghc-7.10.3
 cat >> ~/.bashrc <<EOF
 export PATH="\$HOME/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:\$PATH"
 EOF
@@ -50,15 +44,16 @@ sudo apt-get update && sudo apt-get install stack -y
 
 # Install Chrome
 echo "Installing Google Chrome"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
-sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
 rm -f google-chrome-stable_current_amd64.deb
 apt-get -y -f install
+
 # Install Java 8
-#echo "Installing Java 8"
-#add-apt-repository ppa:webupd8team/java
-#apt-get update
-#apt-get install oracle-java8-installer
-#java -version
-#javac -version
-#apt-get install oracle-java8-set-default
+echo "Installing Java 8"
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
+java -version
+javac -version
+apt-get install oracle-java8-set-default
