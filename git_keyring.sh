9bbdd5529b7d6cccb7a099303923c8c2607c007e
Craig Jacobson <craig.crj@gmail.com>
2019-02-26 13:27:42 -0700

    Fix error in adding credential helper for git keyring

--- git_keyring.sh
+++ git_keyring.sh
@@ -5,5 +5,5 @@ sudo apt -y install git
 sudo apt -y install libsecret-1-0 libsecret-1-dev
 cd /usr/share/doc/git/contrib/credential/libsecret
 sudo make
-git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
 
