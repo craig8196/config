b0eed3908bfe637242be27c2dd3ac48209b2f44f
Craig Jacobson <craig.crj@gmail.com>
2019-02-26 11:51:11 -0700

    Add git keyring setup script

---
+++ git_keyring.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y install git
+sudo apt -y install libsecret-1-0 libsecret-1-dev
+cd /usr/share/doc/git/contrib/credential/libsecret
+sudo make
+git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+
